c6400800626307227b4fcd4ce0984d0acec20257
Robert Krahn <robert.krahn@gmail.com>
2013-08-27 16:12:44 -0700

    cleaning up the manual test script

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
+/*
+ * run with "node test.js user password"
+ */
+
 var util = require("util"),
     hanaInterface = require('./hana-interface');
 
-// see /etc/odbc.ini
-var dsn = 'DSN=hana;'
-        + 'UID=robertkrahn;'
-        + 'PWD=Pa2e476f0';
-
-var session = hanaInterface.getSession({dsn: dsn});
+var user = process.argv[2],
+    password = process.argv[3],
+    // see /etc/odbc.ini
+    dsn = util.format('DSN=hana;UID=%s;PWD=%s', user, password),
+    session = hanaInterface.getSession({dsn: dsn});
 
 session.query('SFLIGHT', "select * from SAPLANE",
               function(err, results) {
                   console.log(JSON.stringify(results, null, 2));
-                  console.log("done");
+		  session.close(function() { console.log('done'); });
               });
